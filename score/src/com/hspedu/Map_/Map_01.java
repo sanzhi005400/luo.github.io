77f3e6db497b5afd2e56f8c35d74705bb66322fa
sanzhi005400 <75853994+sanzhi005400@users.noreply.github.com>
2022-12-28 15:00:51 +0800

    map接口学习

---
+++ score/src/com/hspedu/Map_/Map_01.java
@@ -0,0 +1,19 @@
+package com.hspedu.Map_;
+
+/**
+ * @author: 罗仕杰
+ * @date: 2022/12/28 14:58
+ * @description:
+ */
+/*
+1) Map与Collection并列存在。用于保存具有映射关系的数据:Key-Value
+2)Map 中的 key 和 value 可以是任何引用类型的数据，会封装到HashMap$Node对象中
+3)Map 中的 key 不允许重复，原因和HashSet 一样，前面分析过源码
+4)Map 中的 value 可以重复
+5) Map 的key 可以为 null, value 也可以为null ，注意 key 为null, 只能有一个value 为null ,可以多个.
+6)常用String类作为Map的 key7)key 和 value 之间存在单向一对一关系，即通过指定的 key 总能找到对应的 value*/
+public class Map_01 {
+    public static void main(String[] args) {
+
+    }
+}
